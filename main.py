37547c83856e9b123781ad7276fe4745d72eb323
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-23 19:15:09 +0300

    308. Capture Webcam Video

---
+++ main.py
@@ -0,0 +1,17 @@
+# pip install opencv-python
+import cv2
+import time
+
+video = cv2.VideoCapture(4)
+time.sleep(1)
+while True:
+    check, frame = video.read()
+    cv2.imshow("Capturing", frame)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+video.release()
+
+# print(check1)
+# print(frame1)
